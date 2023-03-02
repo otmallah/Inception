8e780889cae11332a899a37466c03ceb8b8e4f63
Othman Mallah <otmallah@e1r7p6.1337.ma>
2023-03-02 15:59:06 +0100

    mysql

--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -5,6 +5,6 @@ echo 'rc_provide="loopback net"' >> /etc/rc.conf
 /etc/init.d/php-fpm81 start 
 mv wp-config.php /wordpress
 cd wordpress 
-wp core install --url=othman --title=my_site --admin_user=othmanmallah --admin_password=pass --admin_email=othmanmallah13@gmail.com --allow-root
-echo $PWD
+wp core install --url=$DB_USER --title=my_site --admin_user=othmanmallah --admin_password=$DB_PASS --admin_email=othmanmallah13@gmail.com --allow-root
+
 exec $@
