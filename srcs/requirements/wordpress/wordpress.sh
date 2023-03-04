579d91686a4ac3fe8c01658cc7292fec7e957fb7
otmallah <othmanmallah15@gmail.com>
2023-03-04 22:21:13 +0100

    te

--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 sleep 2
-mkdir -p  /home/otmallah/data
-mv wordpress /home/otmallah/data/
 echo 'rc_provide="loopback net"' >> /etc/rc.conf
 /etc/init.d/php-fpm81 start 
-mv wp-config.php /wordpress
-cd wordpress 
+cd /home/otmallah/data/wordpress 
 wp core install --url=$URL_NAME --title=$SB_USER --admin_user=$ADMIN_USER --admin_password=$DB_PASS --admin_email=$USER_EMAIL
 /etc/init.d/php-fpm81 stop
 
