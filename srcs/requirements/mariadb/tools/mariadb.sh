6a013932c595df484da05344b7533ffda9d94ddd
Othman Mallah <otmallah@e1r5p8.1337.ma>
2023-03-09 17:41:50 +0100

    Final project

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,7 +3,7 @@
 /etc/init.d/mariadb setup 
 /etc/init.d/mariadb start 
 mariadb -e "CREATE DATABASE $DB_NAME"
-mariadb -e "CREATE USER 'otmallah'@localhost IDENTIFIED BY 'pass';"
+mariadb -e "CREATE USER '${DB_USER}'@localhost IDENTIFIED BY '${DB_PASS}';"
 mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'; "
 mariadb -e "FLUSH PRIVILEGES"
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nginx
 mkdir -p /home/otmallah/data/wordpress
 echo 'rc_provide="loopback net"' >> /etc/rc.conf
+nginx  -g  'daemon off;'
 exec $@
